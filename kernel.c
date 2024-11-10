6ab2a8a1eff13c7e77a5eee7304d661a1e577ce2
danilodepadua <pwa.danilo@gmail.com>
2024-11-10 20:50:06 -0300

    Modificado os nomes dos arquivos Assembly pois ambos não são compativeis com o cross compiler gcc-mips pra linux. Colocado algumas funcionalidades para o kernel.c porém, ainda não conseguir testar

--- kernel.c
+++ kernel.c
@@ -1,1 +1,22 @@
+#include <stdint.h>
 
+extern void entrada_do_kernel();
+
+void print_string(const char *str) {
+    while (*str) {
+        asm("li $v0, 11");
+        asm("move $a0, %0" : : "r" (*str));
+        asm("syscall");
+        str++;
+    }
+}
+
+void kernel_check() {
+    print_string("Kernel foi aberto\n");
+}
+
+int main() {
+    print_string("Main aberto\n");
+    entrada_do_kernel();
+    return 0;
+}
